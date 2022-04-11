d53f25dab5837e138e2b8802b5b4c32ef7181f2e
Roberto Paes <robertocpaes@gmail.com>
2022-04-11 20:22:42 -0300

    fix

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def connect():
             thread = socketio.start_background_task(background_thread)
     
 if __name__ == '__main__':
-    socketio.run(app,debug=True,host="127.0.0.1", port=8888)
+    socketio.run(app,debug=True,host="0.0.0.0", port=8888)
