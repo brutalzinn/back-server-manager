e7572ee0421a00cd0441ec8e1f3e4d5045fbc341
Roberto Paes <robertocpaes@gmail.com>
2022-04-16 11:36:43 -0300

    deploy

--- main.py
+++ main.py
@@ -18,44 +18,24 @@ threads = []
 psutil.PROCFS_PATH = '/rootfs/proc'
 @socketio.on('connect')
 def connect():
-
     if "Api-Key" not in request.headers or request.headers.get('Api-Key') != os.getenv('API_KEY'):
         print("sem api key.")
         socketio.emit("info", {"message":"Você não possui permissão para acessar esse servidor."})
         disconnect()
         return
-        
+    socketio.emit("info", {"message":"TESTANDOOOOOOOOOOOOOOOOOOOO."})
+
     threads.append(Worker("server_process_list", request.sid, socketio))
     threads.append(Worker("server_stats", request.sid, socketio))
 
     print(f'Crio {len(threads)} threads para o ' + request.sid)
 
 
-def checkThreadIsRunning(channel) -> bool:
-    for w in threads:
-        if w.socket_channel == channel and w.switch:
-            return True
-    return False
-
-def killThreadByChannel(channel) -> bool:
-    for w in threads:
-        if w.socket_channel == channel:
-            w.stop()
 
 def getThreadByChannel(channel:str , socket_id: str) -> Worker:
     for w in threads:
         if w.socket_channel == channel.lower() and w.socket_id.lower() == socket_id.lower():
             return w
-
-def removeThreads(channel, socket_id):
-    removidas = 0
-    for t in threads: 
-        exist = t.channel == channel and socket_id.lower() == t.socket_id.lower()
-        if exist : 
-            removidas += 1
-            # print("THREADS REMOVIDAS: " + str(removidas))
-            t.stop()
-            threads.remove(t)
 
 def removeAllThreads(socket_id :str):
     print(f"Preparando para remover.. {len(threads)}")
@@ -96,6 +76,14 @@ def process(json):
     worker = restartThreadByChannel("server_process_list", request.sid)
     worker.do_work(processes_thread, json)
 
+@socketio.on('process_kill')
+def process(json):
+    try:
+        p = psutil.Process(json["pid"])
+        p.terminate()
+        socketio.emit("info", {"message":f"Processo {p.name()}({p.pid}) finalizado."})
+    except:
+        socketio.emit("info", {"message":"PID não existe."})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host=os.getenv('HOST') or "0.0.0.0",port=os.getenv('FLASK_PORT') or 5555)
